7e38dc4ba91dc4725d6165f944fa0c683b5dc5b5
p4w3l <pawel.nieduzak@icloud.com>
2022-09-06 19:38:33 +0200

    finished project

--- src/js/main.js
+++ src/js/main.js
@@ -24,7 +24,7 @@ const popUps = {
 	pauseGame: document.querySelector('.main__popup--pausePop'),
 	winPop: document.querySelector('.main__popup--win'),
 };
-
+const dice = document.querySelectorAll('.dice');
 const winner = document.querySelector('.main__player');
 const { shadow, gameRules, pauseGame, winPop } = popUps;
 const { playboard, desktop, topPart, bottomPart } = playboards;
@@ -33,7 +33,7 @@ const { topScrP1, topScrP2, curScrP1, curScrP2 } = numbersVar;
 
 let arr = [];
 let number, sum;
-let finalScore = 10;
+let finalScore = 100;
 
 const enterGame = () => {
 	shadow.classList.add('hidden');
@@ -43,75 +43,134 @@ const enterGame = () => {
 	winPop.classList.add('hidden');
 	bottomPart.classList.remove('current-player');
 };
+
 const pauseGameFunction = () => {
 	shadow.classList.remove('hidden');
 	pauseGame.classList.remove('hidden');
 };
+
 const showWinPop = () => {
 	shadow.classList.remove('hidden');
 	winPop.classList.remove('hidden');
 };
+
 const resetGame = () => {
 	topScrP1.textContent = 0;
 	topScrP2.textContent = 0;
 	curScrP1.textContent = 0;
 	curScrP2.textContent = 0;
 	arr = [];
-
+	addHiddenDice();
 	enterGame();
 };
+
 const changePlayer = () => {
 	arr = [];
 	topPart.classList.toggle('current-player');
 	bottomPart.classList.toggle('current-player');
 };
+
 const randomNumber = () => {
 	return (number = Math.trunc(Math.random() * 6 + 1));
 };
+
 const holdFunction = () => {
-	if (topPart.classList.contains('current-player')) {
-		topScrP1.textContent =
-			Number(curScrP1.textContent) + Number(topScrP1.textContent);
-		curScrP1.textContent = 0;
-		if (Number(topScrP1.textContent) >= finalScore) {
+	addHiddenDice();
+	const calculateTopScr = (topScr, curScr) => {
+		topScr.textContent =
+			Number(curScr.textContent) + Number(topScr.textContent);
+		curScr.textContent = 0;
+		if (Number(topScr.textContent) >= finalScore) {
 			showWinPop();
 			winner.textContent = 'First ';
 		}
+	};
+
+	if (topPart.classList.contains('current-player')) {
+		calculateTopScr(topScrP1, curScrP1);
 	} else {
-		topScrP2.textContent =
-			Number(curScrP2.textContent) + Number(topScrP2.textContent);
-		curScrP2.textContent = 0;
-		if (Number(topScrP2.textContent) >= finalScore) {
-			showWinPop();
-			winner.textContent = 'Second ';
-		}
+		calculateTopScr(topScrP2, curScrP2);
 	}
 	changePlayer();
 };
 
+const addHiddenDice = () => {
+	dice.forEach(el => {
+		el.classList.add('hidden');
+	});
+};
+const appropriateDice = () => {
+	const addHiddenDice = element => {
+		dice.forEach(el => {
+			el.classList.add('hidden');
+		});
+		element.classList.remove('hidden');
+	};
+	switch (
+		number === 1 ||
+		number === 2 ||
+		number === 3 ||
+		number === 4 ||
+		number === 5 ||
+		number === 6
+	) {
+		case number === 1:
+			addHiddenDice(dice[0]);
+
+			break;
+		case number === 2:
+			addHiddenDice(dice[1]);
+
+			break;
+		case number === 3:
+			addHiddenDice(dice[2]);
+
+			break;
+		case number === 4:
+			addHiddenDice(dice[3]);
+
+			break;
+		case number === 5:
+			addHiddenDice(dice[4]);
+
+			break;
+		case number === 6:
+			addHiddenDice(dice[5]);
+
+			break;
+	}
+};
+
 const rollDice = () => {
 	randomNumber();
-	console.log(number);
+	appropriateDice();
+
+	const calculateScr = curScr => {
+		arr.push(number);
+		sum = arr.reduce((a, b) => a + b, 0);
+		curScr.textContent = sum;
+	};
+
+	const drawnOne = curScr => {
+		arr = [];
+		curScr.textContent = 0;
+		setTimeout(() => {
+			dice[0].classList.add('hidden');
+		}, 500);
+		changePlayer();
+	};
+
 	if (topPart.classList.contains('current-player')) {
 		if (number >= 2) {
-			console.log(arr);
-			arr.push(number);
-			sum = arr.reduce((a, b) => a + b, 0);
-			curScrP1.textContent = sum;
+			calculateScr(curScrP1);
 		} else {
-			arr = [];
-			curScrP1.textContent = 0;
-			changePlayer();
+			drawnOne(curScrP1);
 		}
 	} else {
 		if (number >= 2) {
-			arr.push(number);
-			sum = arr.reduce((a, b) => a + b, 0);
-			curScrP2.textContent = sum;
+			calculateScr(curScrP2);
 		} else {
-			arr = [];
-			curScrP2.textContent = 0;
-			changePlayer();
+			drawnOne(curScrP2);
 		}
 	}
 };
@@ -126,14 +185,3 @@ for (const btn of resetBtn) {
 holdBtn.addEventListener('click', holdFunction);
 rollBtn.addEventListener('click', rollDice);
 pauseBtn.addEventListener('click', pauseGameFunction);
-
-// window.addEventListener('resize', function () {
-
-// 	if (window.matchMedia('(min-width: 500px)').matches) {
-// 		playboard.classList.add('hidden');
-// 		desktop.classList.remove('hidden');
-// 	} else {
-// 		mobile.classList.remove('hidden');
-// 		desktop.classList.add('hidden');
-// 	}
-// });
